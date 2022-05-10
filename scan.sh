f86e0aebe59865936abfdf445cf4636c018c105d
MejiasXP <40152882+MejiasXP@users.noreply.github.com>
2022-05-10 18:36:55 +0200

    Create scan.sh

---
+++ scan.sh
@@ -0,0 +1,65 @@
+#!/system/bin/sh
+
+clear
+
+echo -e "\e[31mESCANEO DE REDES\e[0m"
+
+echo ""
+echo -n "Objetivo: "
+read target
+echo ""
+echo -n "Puerto: "
+read port
+
+
+
+function menu() {
+echo ""
+echo -e "\e[32m=========================\e[0m"
+echo ""
+echo -e "Objetivo \e[32m$target\e[0m"
+echo ""
+echo -e "Puerto \e[32m$port\e[0m"
+echo ""
+echo -e "\e[32m=========================\e[0m"
+echo ""
+echo "Tipo de análisis"
+echo "1. Escaneo básico"
+echo "2. Escaneo completo"
+echo "3. Vulnerabilidades"
+echo "4. XSS"
+echo ""
+echo -n "Opción: "
+read opt
+
+if [ $opt == 1 ]
+then
+echo -e "\e[32m=========================\e[0m"        
+nmap $target
+menu
+echo "a"
+
+elif [ $opt == 2 ]
+then
+echo -e "\e[32m=========================\e[0m"            
+sudo nmap -sS $target
+menu            
+exit
+
+elif [ $opt == 3 ]
+then
+echo -e "\e[32m=========================\e[0m"                
+nmap -sV --script vulnerscan-master/vulscan.nse $target
+menu
+exit
+
+elif [ $opt == 3 ]
+then
+echo -e "\e[32m=========================\e[0m"                
+nmap -sV --script vulnerscan-master/vulscan.nse $target
+menu
+exit                
+                
+fi 
+}
+menu
