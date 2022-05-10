6c2150faba89de7f01ade713b430e9a30f526eef
MejiasXP <40152882+MejiasXP@users.noreply.github.com>
2022-05-10 19:21:15 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 apt-get update
+pkg update
 apt install nmap
+pkg install python
 mkdir utilidades
 cd utilidades
 git clone https://github.com/sherlock-project/sherlock
 cd sherlock
-pip -r install requirements.txt
+pip install -r requirements.txt
 cd -
 cd -
